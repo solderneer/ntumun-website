f0308432324c6dfbb12563963d4c454133c4e051
Sudharshan <solderneer@gmail.com>
2019-11-13 19:29:11 +0800

    feat: added parcel, postcss

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+{
+  "modules": true,
+  "plugins": {
+    "autoprefixer": {
+      "grid": true
+    },
+    "postcss-font-magician": {
+         variants: {
+             "Roboto Condensed": {
+                 "300": [],
+                 "400": [],
+                 "700": []
+             }
+         },
+         foundries: ["google"]
+    }
+  }
+}
